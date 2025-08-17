d0248778ceada5960f91826591e4f4638432df20
fethulmubin <fetihul@gmail.com>
2025-08-17 20:24:53 +0300

    about section with ui banto-grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,52 +1,66 @@
-import { cn } from "../lib/utils";
-import { ArrowUpRight } from 'lucide-react';
-import MajicButton from "./ui/MajicButton";
+
+
+import { ArrowUpRight } from "lucide-react";
+import MajicButton from "../components/ui/MajicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
-export default function Hero() {
+const Hero = () => {
   return (
-    <div className="pb-20 pt-36 h-screen">
+    <div className="pb-20 pt-36">
+      {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
         <Spotlight
-          className="top-10 left-full w-[50vw] h-[80vh]"
+          className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
-        <Spotlight className="top-28 left-80 w-[50vw] h-[80vh]" fill="blue" />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className=" flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 absolute top-0 left-0">
-        <div
-          className={cn(
-            "absolute inset-0",
-            "[background-size:40px_40px]",
-            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
-          )}
-        />
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
         {/* Radial gradient for the container to give a faded look */}
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
 
-        <div className="flex justify-center relative my-20 z-10">
-          <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col justify-center items-center'>
-            <TextGenerateEffect words="Transforming Concepts Into Seamless Experiences"
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+          </p>
+
+          <a href="#about">
+            <MajicButton
+              title="Show my work"
+              icon={<ArrowUpRight />}
+              position="right"
             />
-
-            <p className="text-center text-white text-sm md:text-lg lg:text-2xl md:tracking-wider my-4 ">
-              Hi I&apos;m Fethulmubin a passionate full-stack developer based in Ethiopia.
-            </p>
-
-            <a href="">
-                <MajicButton title='Show my work' icon={<ArrowUpRight />} position="right"/>
-            </a>
-          </div>
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Hero;
